e4cc558ae6b7541a80caa664cd50144347cd208f
Lorenzo <testlorenzo25@gmail.com>
2025-09-09 15:00:22 +0200

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -57,6 +57,11 @@ export default function Hero({ waitlistPeople }: { waitlistPeople: number }) {
         </div>
       </div>
 
+      {/* Form */}
+      <div className="w-full max-w-md mt-6">
+        <Form onSuccessChange={setIsSuccess} />
+      </div>
+
       {/* Indicatore di scroll */}
       <div className="hidden md:block absolute bottom-9 left-1/2 transform -translate-x-1/2 animate-bounce">
         <div className="w-6 h-10 border-2 border-gray-400 rounded-full flex justify-center">
