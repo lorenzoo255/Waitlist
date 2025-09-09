7b7b4ecf2ba63e94bc31551e155c159d3d22bf6c
Lorenzo <testlorenzo25@gmail.com>
2025-09-09 11:33:04 +0200

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -50,6 +50,11 @@ export default function Hero({ waitlistPeople }: { waitlistPeople: number }) {
         </p>
       </div>
 
+      {/* Persone in lista */}
+      <div className="flex items-center justify-center gap-2 px-4 mt-2">
+        <People count={waitlistPeople} />
+      </div>
+
       {/* Indicatore di scroll */}
       <div className="hidden md:block absolute bottom-9 left-1/2 transform -translate-x-1/2 animate-bounce">
         <div className="w-6 h-10 border-2 border-gray-400 rounded-full flex justify-center">
